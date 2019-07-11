8e9d2b3bada1d65ebc2593f48bb61a3b4a8966f9
iKilipeesy <kilipisi@gmail.com>
2019-07-11 14:08:53 +0200

    jo

--- js/csgoRoulette.js
+++ js/csgoRoulette.js
@@ -1,6 +1,7 @@
 // Variables
 var ctPlayer;
 var activeWeaponCategeory;
+
 // Arrays, enum
 const weaponCategories = {
 	RIFLE: "rifle",
@@ -16,12 +17,20 @@ var rifle = [ "AK47", "Galil", "M4", "Famas", "Scout", "Awp", "Autosniper",
 var heavy = [ "Negev", "M249", "Nova", "XM1014", "MAG-7/Pump" ];
 
 var pistol = [ "Deagle", "Dual Berretas", "USP/P2K/Glock", "P250", 
-		"CZ/Five-Seven/Tec-9" ];
+				"CZ75-Auto/Five-Seven/Tec-9" ];
 
 var smg = [ "UMP-45", "MP5/MP7", "PP-Bizon", "MP9/MAC-10", "P90" ];
 
 var grenades = [ "HE", "Incendiary", "Smoke", "Flash", "Decoy" ];
 
+
+/*
+
+*/
+function startRoulette(){
+	var activeWeapons = randomWeapon(amountOfWeapons());
+	console.log(activeWeapons);
+}
 
 /*
 Returns a random weapon category
@@ -31,35 +40,59 @@ function randomWeaponCategory(){
 	var randomNumber = Math.floor(Math.random() * 100);
 	
 	if(randomNumber <= 20){
-		weaponCategeory = weaponCategories.HEAVY;
+		weaponCategory = weaponCategories.HEAVY;
 	}
 	else if(randomNumber <= 55){
-		weaponCategeory = weaponCategories.RIFLE;
+		weaponCategory = weaponCategories.RIFLE;
 	}
 	else if(randomNumber <= 75){
-		weaponCategeory = weaponCategories.SMG;
+		weaponCategory = weaponCategories.SMG;
 	}
 	else if(randomNumber <= 100){
-		weaponCategeory = weaponCategories.PISTOL;
+		weaponCategory = weaponCategories.PISTOL;
 	}
 	
-	return weaponCategeory;
+	return weaponCategory;
 }
 
-function randomWeapon(weaponCategory){
-	var randomNumber = Math.floor(Math.random() * 100);
+/*
 
-	if(weaponCategory == weaponCategories.RIFLE){
+*/
+function randomWeapon(amountOfWeapons){
+	var weapons =  new Array();
 
+	weapons[0] = pistol[Math.floor(Math.random() * 5)];
+
+	if (amountOfWeapons == 2) {
+		var weaponCategory = randomWeaponCategory();
+
+		if(weaponCategory == weaponCategories.RIFLE){
+			weapons[1] = rifle[Math.floor(Math.random() * 8)];
+		}
+		if(weaponCategory == weaponCategories.HEAVY){
+			weapons[1] = heavy[Math.floor(Math.random() * 5)];
+		}
+		if(weaponCategory == weaponCategories.SMG){
+			weapons[1] = smg[Math.floor(Math.random() * 5)];
+		}	
 	}
-	if(weaponCategory == weaponCategories.HEAVY){
 
-	}
-	if(weaponCategory == weaponCategories.PISTOL){
-
-	}
-	if(weaponCategory == weaponCategories.SMG){
-
+	if (Math.floor(Math.random() * 100) >= 90) {
+		weapons[2] = "Zeus";
 	}
 	
+	return weapons;
+}
+
+/*
+
+*/
+function amountOfWeapons(){
+
+	if (Math.floor(Math.random() * 100) <= 80) {
+		return 2;
+	}
+	else{
+		return 1;
+	}
 }
