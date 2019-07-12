29a0f8b1cb66655270f811e8b717e0beec854dd1
iKilipeesy <kilipisi@gmail.com>
2019-07-12 14:20:58 +0200

    jo

--- js/csgoRoulette.js
+++ js/csgoRoulette.js
@@ -1,5 +1,5 @@
 // Variables
-var ctPlayer;
+var ct = true;
 var activeWeaponCategeory;
 
 // Arrays, enum
@@ -11,12 +11,11 @@ const weaponCategories = {
 	GRENADE: "grenade"
 }
 
-var rifle = [ "AK47", "Galil", "M4", "Famas", "Scout", "Awp", "Autosniper", 
-		"Aug/Krieg" ];
+var rifle = [ "AK47/M4", "Galil/Famas", "Scout/SSG 08", "Awp", "Autosniper", "Aug/Krieg" ];
 
 var heavy = [ "Negev", "M249", "Nova", "XM1014", "MAG-7/Pump" ];
 
-var pistol = [ "Deagle", "Dual Berretas", "USP/P2K/Glock", "P250", 
+var pistol = [ "Deagle/R8 Revolver", "Dual Berretas", "USP/P2K/Glock", "P250", 
 				"CZ75-Auto/Five-Seven/Tec-9" ];
 
 var smg = [ "UMP-45", "MP5/MP7", "PP-Bizon", "MP9/MAC-10", "P90" ];
@@ -25,16 +24,49 @@ var grenades = [ "HE", "Incendiary", "Smoke", "Flash", "Decoy" ];
 
 
 /*
+*
 Starts the roulette
+*
 */
 function startRoulette(){
 	var activeWeapons = randomWeapon(amountOfWeapons());
 	console.log(activeWeapons);
+
+	//Change the text of the span to the randomly generated pistol
+	document.getElementById("secondaryWeaponName").innerHTML = activeWeapons[0];
+
+	//Change the text of the span to the randomly generated primary if there is one
+	if (activeWeapons[1] != null) {
+		document.getElementById("primaryWeaponName").innerHTML = activeWeapons[1];
+	}
+	else{
+		document.getElementById("primaryWeaponName").innerHTML = "None"
+	}
+
 }
 
 /*
+*
+
+*
+*/
+function changeSide(){
+	if (ct == true) {
+		document.getElementById("changeSideButton").style.backgroundImage = "url(resources/csgo/tIcon.png)"
+		ct = false;
+	}
+	else{
+		document.getElementById("changeSideButton").style.backgroundImage = "url(resources/csgo/ctIcon.png)"
+		ct = true;
+	}
+}
+
+
+/*
+*
 Returns a random weapon category
- */
+*
+*/
 function randomWeaponCategory(){
 	var weaponCategory;
 	var randomNumber = Math.floor(Math.random() * 100);
@@ -52,25 +84,89 @@ function randomWeaponCategory(){
 	return weaponCategory;
 }
 
+
 /*
+*
 Returns the weapons the user has to buy
+*
 */
 function randomWeapon(amountOfWeapons){
 	var weapons =  new Array();
 
 	weapons[0] = pistol[Math.floor(Math.random() * 5)];
 
+	if (weapons[0] == pistol[2]) {
+		if (ct == true) {
+			weapons[0] = "USP-S/P2000";
+		}
+		else{
+			weapons[0] = "Glock-18";
+		}
+	}
+	if (weapons[0] == pistol[4]) {
+		if (ct == true) {
+			weapons[0] = "CZ75-Auto/Five-Seven";
+		}
+		else{
+			weapons[0] = "CZ75-Auto/Tec-9";
+		}
+	}
+
 	if (amountOfWeapons == 2) {
 		var weaponCategory = randomWeaponCategory();
 
 		if(weaponCategory == weaponCategories.RIFLE){
-			weapons[1] = rifle[Math.floor(Math.random() * 8)];
+			weapons[1] = rifle[Math.floor(Math.random() * 6)];
+
+			//Changes the weapon to either a ct or t side weapon
+			if (weapons[1] == rifle[0]) {
+				if (ct == true) {
+					weapons[1] = "M4A4/M4A1-S";
+				}
+				else{
+					weapons[1] = "AK-47";
+				}
+			}
+			if (weapons[1] == rifle[1]) {
+				if (ct == true) {
+					weapons[1] = "Famas";
+				}
+				else{
+					weapons[1] = "Galil";
+				}
+			}
+			if (weapons[1] == rifle[5]) {
+				if (ct == true) {
+					weapons[1] = "Aug";
+				}
+				else{
+					weapons[1] = "SG 553";
+				}
+			}
 		}
 		if(weaponCategory == weaponCategories.HEAVY){
 			weapons[1] = heavy[Math.floor(Math.random() * 5)];
+
+			if (weapons[1] == heavy[4]) {
+				if (ct == true) {
+					weapons[1] = "MAG-7";
+				}
+				else{
+					weapons[1] = "Pump action";
+				}
+			}
 		}
 		if(weaponCategory == weaponCategories.SMG){
 			weapons[1] = smg[Math.floor(Math.random() * 5)];
+
+			if (weapons[1] == smg[3]) {
+				if (ct == true) {
+					weapons[1] = "MP9";
+				}
+				else{
+					weapons[1] = "MAC-10";
+				}
+			}
 		}	
 	}
 
@@ -83,9 +179,12 @@ function randomWeapon(amountOfWeapons){
 	return weapons;
 }
 
+
 /*
+*
 Decides if the user has to buy a primary weapon
 Returns the amount of weapons
+*
 */
 function amountOfWeapons(){
 
